4750a65ea42f65b094fb3ae7dbf7fa4c5d7830a0
shinaayomi <lateef718@gmail.com>
2023-09-24 10:30:06 +0100

    Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,68 @@
 import Image from "next/image";
 import logo from "../../images/logo.png";
+import cartIcon from "../../images/cartIcon.png";
+import { SlLocationPin } from "react-icons/sl";
+import { HiOutlineSearch } from "react-icons/hi";
+import { BiCaretDown } from "react-icons/bi";
 
 const Header = () => {
   return (
-    <div>
-      <div>
-        <Image src={logo} alt="logoImage" />
+    <div className="w-full h-20 bg-amazon_blue text-lightText sticky top-0 z-50">
+      <div className="w-full h-full mx-auto inline-flex items-center justify-between gap-1 mdl:gap-3 px-4">
+        {/* logo */}
+        <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 flex items-center justify-center h-[70%]">
+          <Image
+            className="w-28 object-cover mt-1"
+            src={logo}
+            alt="logoImage"
+          />
+        </div>
+        {/* delivery */}
+        <div className="px-2 border border-transparent hover:border-white cursor-pointer duration-300 hidden xl:inline-flex items-center justify-center gap-1 h-[70%]">
+          <SlLocationPin />
+          <div className="text-xs">
+            <p>Delivery to</p>
+            <p className="text-white font-bold uppercase">USA</p>
+          </div>
+        </div>
+        {/* searchbar */}
+        <div className="flex-1 h-10 hidden xl:inline-flex items-center justify-between relative">
+          <input
+            className="w-full h-full rounded-md px-2 placeholder:text-sm text-base text-black border-[3px] border-transparent outline-none focus-visible:border-amazon_yellow"
+            type="text"
+            placeholder="Search on Adebisi amazon products"
+          />
+          <span className="w-12 h-full bg-amazon_yellow text-black text-2xl flex justify-center items-center absolute right-0 rounded-tr-md rounded-br-md">
+            <HiOutlineSearch />
+          </span>
+        </div>
+        {/* signin */}
+        <div className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%]">
+          <p>Hello, sign in</p>
+          <p className="text-white font-bold flex items-center ">
+            Account & Lists{" "}
+            <span>
+              <BiCaretDown />
+            </span>
+          </p>
+        </div>
+        {/* favourite */}
+        <div className="text-xs text-gray-100 flex flex-col justify-center px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%]">
+          <p>Marked</p>
+          <p className="text-white font-bold">& Favourite</p>
+        </div>
+        {/* cart */}
+        <div className="flex items-center  px-2 border border-transparent hover:border-white cursor-pointer duration-300 h-[70%] relative">
+          <Image
+            className="w-auto object-cover h-8"
+            src={cartIcon}
+            alt="cartImg"
+          />
+          <p className="text-xs text-white font-bold mt-3">Cart</p>
+          <span className="absolute top-2 left-[29px] text-amazon_yellow text-xs font-semibold">
+            0
+          </span>
+        </div>
       </div>
     </div>
   );
