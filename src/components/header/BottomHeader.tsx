b0a9c1e565e15c6a1fb01b42b34129f6c0c4048e
shinaayomi <lateef718@gmail.com>
2023-10-20 18:18:55 +0100

    header bottom

--- src/components/header/BottomHeader.tsx
+++ src/components/header/BottomHeader.tsx
@@ -4,9 +4,27 @@ import { LuMenu } from "react-icons/lu";
 const BottomHeader = () => {
   return (
     <div className="w-full h-10 bg-amazon_light text-sm text-white px-4 flex items-center">
-      <p className="flex items-center gap-1 h-8 border border-transparent hover:border-whitecursor-pointer duration-300">
-        <LuMenu />
+      <p className="flex items-center gap-1 h-8 border border-transparent hover:border-white px-2 cursor-pointer duration-300">
+        <LuMenu className="text-xl" />
         <span>All</span>
+      </p>
+      <p className="hidden md:inline-flex items-center h-8 border border-transparent hover:border-white px-2 cursor-pointer duration-300">
+        Todays Deals
+      </p>
+      <p className="hidden md:inline-flex items-center h-8 border border-transparent hover:border-white px-2 cursor-pointer duration-300">
+        Customer Service
+      </p>
+      <p className="hidden md:inline-flex items-center h-8 border border-transparent hover:border-white px-2 cursor-pointer duration-300">
+        Registry
+      </p>
+      <p className="hidden md:inline-flex items-center h-8 border border-transparent hover:border-white px-2 cursor-pointer duration-300">
+        Gift Cards
+      </p>
+      <p className="hidden md:inline-flex items-center h-8 border border-transparent hover:border-white px-2 cursor-pointer duration-300">
+        Sell
+      </p>
+      <p className="hidden md:inline-flex items-center h-8 text-amazon_yellow hover:text-red-400 border border-transparent hover:border-red-600 px-2 cursor-pointer duration-300">
+        Sign Out
       </p>
     </div>
   );
