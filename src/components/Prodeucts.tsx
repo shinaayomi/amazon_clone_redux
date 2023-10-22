0e81fb1fe064e08c19717c18a3f6ecfbaa749517
shinaayomi <lateef718@gmail.com>
2023-10-22 03:11:44 +0100

    product component

--- src/components/Prodeucts.tsx
+++ src/components/Prodeucts.tsx
@@ -7,7 +7,7 @@ import { FaHeart } from "react-icons/fa";
 const Products = ({ productData }: any) => {
   console.log("__", productData);
   return (
-    <div className="w-full px-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
+    <div className="w-full px-4 sm:px-6 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-6">
       {productData.map(
         ({
           _id,
@@ -22,7 +22,7 @@ const Products = ({ productData }: any) => {
         }: ProductProps) => (
           <div
             key={_id}
-            className="w-full bg-white text-black border border-gray-300 rounded-lg p-4 group"
+            className="w-full bg-white text-black border border-gray-300 rounded-lg p-4 group overflow-hidden"
           >
             <div className="w-full h-[260px] relative">
               <Image
@@ -33,11 +33,11 @@ const Products = ({ productData }: any) => {
                 height={300}
                 priority
               />
-              <div className="w-12 h-24 absolute bottom-10 right-0 border border-gray-400 bg-white rounded-md flex flex-col">
-                <span className="w-full h-full border-b border-gray-400 flex items-center justify-center text-xl">
+              <div className="w-12 h-24 absolute bottom-10 right-0 border border-gray-400 bg-white rounded-md flex flex-col translate-x-20 group-hover:translate-x-0 transition-transform duration-300">
+                <span className="w-full h-full border-b border-gray-400 flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300">
                   <HiShoppingCart />
                 </span>
-                <span>
+                <span className="w-full h-full flex items-center justify-center text-xl bg-transparent hover:bg-amazon_yellow cursor-pointer duration-300">
                   <FaHeart />
                 </span>
               </div>
